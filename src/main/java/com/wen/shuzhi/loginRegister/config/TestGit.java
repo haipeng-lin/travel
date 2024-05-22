26405454e38dac813c1604455d28f11d70c1fc64
haipeng-lin <haipeng_lin@163.com>
2024-05-22 17:42:44 +0800

    测试idea提交代码

---
+++ src/main/java/com/wen/shuzhi/loginRegister/config/TestGit.java
@@ -0,0 +1,11 @@
+package com.wen.shuzhi.loginRegister.config;
+
+/**
+ * @Author Linhaipeng
+ * @Mailbox haipeng_lin@163.com
+ * @Date 2024/5/22 17:28
+ * @Version 1.0
+ */
+
+public class TestGit {
+}
